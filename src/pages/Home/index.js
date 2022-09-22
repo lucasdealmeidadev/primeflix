cd5c59c786f66a86ffa5f2d5989d69d46218f6e0
Lucas de Almeida <lucasdealmeida.monteiro@gmail.com>
2022-09-21 22:20:49 -0300

    Customizing the project's CSS

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -98,7 +98,7 @@ function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        <div className={styles.container_title}>
+        <div className={styles.grid_title}>
           <h1>Bem-Vindo(a).</h1>
           <p>Milhões de Filmes, Séries e Pessoas para Descobrir. Explore já.</p>
           <form onSubmit={handleSubmit}>
