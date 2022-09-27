159645d17fa329a175472ba1d85b69b5d27f6933
Lucas de Almeida <lucasdealmeida.monteiro@gmail.com>
2022-09-26 22:37:40 -0300

    Adding environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
 	baseURL: 'https://api.themoviedb.org/3/',
 	params: {
-		api_key: '29635cea469d75e2285ac15ee8e2356e',
+		api_key: process.env.REACT_APP_API_KEY,
 		language: 'pt-BR'
 	}
 });
